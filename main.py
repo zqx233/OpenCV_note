3edb1785717d0ad04b260fafd1c0e86d16ebd646
xin <zhaoqixin518@gmail.com>
2022-03-18 21:32:57 +0800

    第五天学习

--- main.py
+++ main.py
@@ -292,5 +292,58 @@ def norm_demo():
     cv.destroyAllWindows()
 
 
+def resize_demo():
+    image = cv.imread("D:/Code/py/OpenCV/src/1.jpg")
+    h, w, c = image.shape
+    cv.namedWindow("resize", cv.WINDOW_AUTOSIZE)
+    dst = cv.resize(image, (0, 0), fx=0.75, fy=0.5, interpolation=cv.INTER_NEAREST)
+    cv.imshow("resize", dst)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+def flip_demo():
+    image = cv.imread("D:/Code/py/OpenCV/src/1.jpg")
+    cv.imshow("image", image)
+    cv.namedWindow("flip", cv.WINDOW_AUTOSIZE)
+    dst = cv.flip(image, 0)
+    cv.imshow("flip", dst)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+def rotate_demo():
+    src = cv.imread("D:/Code/py/OpenCV/src/1.jpg")
+    h, w, c = src.shape
+    M = np.zeros((2, 3), dtype=np.float32)
+    alpha = np.cos(np.pi / 4.0)
+    beta = np.sin(np.pi / 4.0)
+    print("alpha : ", alpha)
+
+    # 初始旋转矩阵
+    M[0, 0] = alpha
+    M[1, 1] = alpha
+    M[0, 1] = beta
+    M[1, 0] = -beta
+    cx = w / 2
+    cy = h / 2
+    tx = (1 - alpha) * cx - beta * cy
+    ty = beta * cx + (1 - alpha) * cy
+    M[0, 2] = tx
+    M[1, 2] = ty
+
+    # change with full size
+    bound_w = int(h * np.abs(beta) + w * np.abs(alpha))
+    bound_h = int(h * np.abs(alpha) + w * np.abs(beta))
+
+    # 添加中心位置迁移
+    M[0, 2] += bound_w / 2 - cx
+    M[1, 2] += bound_h / 2 - cy
+    dst = cv.warpAffine(src, M, (bound_w, bound_h))
+    cv.imshow("rotate without cropping", dst)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
 if __name__ == "__main__":
-    norm_demo()
+    rotate_demo()
